bb6863e63ae37bb18e9c0131697f2bcdb78bbf5b
wuchengji <76484614@qq.com>
2018-09-10 17:04:09 +0800

    refactor: 将交易参数模块单独抽离出来

--- src/components/common/Sidebar.vue
+++ src/components/common/Sidebar.vue
@@ -249,6 +249,17 @@
                         ]
                     },
                     {
+                        icon: 'icon-huiyuanzhanghaoguanli',
+                        index: '11',
+                        title: '交易基础参数设置',
+                        subs: [
+                            {
+                                index: 'baseParamsSet',
+                                title: '交易基础参数设置'
+                            }
+                        ]
+                    },
+                    {
                         icon: 'icon-yunying',
                         index: '9',
                         title: '运营管理',
@@ -264,10 +275,6 @@
                             // {
                             //     index: "currencyRatio",
                             //     title: "货币比例设置"
-                            // },
-                            // {
-                            //     index: "baseParamsSet",
-                            //     title: "交易基础参数设置"
                             // },
                             // {
                             //     index: "topicManage",
