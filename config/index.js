c96e2944fb1fdacece1c68c762d686f517c339a3
“chang.w” <185483658@qq.com>
2018-09-19 09:05:51 +0800

    refactor: 页面重构

--- config/index.js
+++ config/index.js
@@ -9,12 +9,8 @@ const path = require('path');
 // let baseURL = "http://172.16.10.56";
 // let baseURL = "http://172.16.10.253";
 // const baseURL = 'http://172.16.10.19';
-
-// const baseURL = 'http://172.16.10.100';
-
 const baseURL = 'http://172.16.10.12';
-// const baseURL = 'http://172.16.10.100';
-// const baseURL = 'https://test.dnvhot.tech';
+// const baseURL = 'https:/、/test.dnvhot.tech';
 
 // 测试地址不需要端口号
 const url_1 = baseURL + ':8190';
