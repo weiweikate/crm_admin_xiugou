54621fc6ca17bb63d4bebb306ff11491f517cc0e
yudeguo <549863285@qq.com>
2018-11-12 10:53:16 +0800

    feactor:添加webpack dll配置

--- build/webpack.dll.conf.js
+++ build/webpack.dll.conf.js
@@ -2,28 +2,28 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: {
-    vendor: ['vue/dist/vue.common.js','vue-router', 'babel-polyfill','axios']
-  },
-  output: {
-    path: path.join(__dirname, '../static/js'),
-    filename: '[name].dll.js',
-    library: '[name]_library'
-  },
-  plugins: [
-    new webpack.DllPlugin({
-      path: path.join(__dirname, '.', '[name]-manifest.json'),
-      name: '[name]_library'
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    })
-  ]
+    entry: {
+        vendor: ['vue/dist/vue.common.js','vue-router', 'babel-polyfill','axios']
+    },
+    output: {
+        path: path.join(__dirname, '../static/js'),
+        filename: '[name].dll.js',
+        library: '[name]_library'
+    },
+    plugins: [
+        new webpack.DllPlugin({
+            path: path.join(__dirname, '../dist', '[name]-manifest.json'),
+            name: '[name]_library'
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        })
+    ]
 };
