979d3398eb41b13341ae9afadcbae26bb1251050
wuchengji <76484614@qq.com>
2018-11-14 17:15:59 +0800

    refactor: 修复订单字段bug

--- src/components/page/OrderManage/OrderManage/OrderInfo.vue
+++ src/components/page/OrderManage/OrderManage/OrderInfo.vue
@@ -128,9 +128,8 @@
                     </el-table-column>
                     <el-table-column prop="id" label="支付详情" align="center">
                         <template slot-scope="scope">
-                            代币支付:{{scope.row.tokenCoin}}枚<br/>
                             余额支付:{{scope.row.balance | handleMoney}}<br/>
-                            积分抵扣:{{scope.row.userScore}}<br/>
+                            1元券抵扣:{{scope.row.tokenCoin}}<br/>
                             优惠券抵扣:{{scope.row.couponPrice | handleMoney}}<br/>
                             <template v-if='(scope.row.type & 1) != 0'>平台支付</template>
                             <template v-else-if='(scope.row.type & 2) != 0'>微信小程序 </template>
