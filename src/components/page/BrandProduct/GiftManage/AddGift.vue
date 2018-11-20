590654b91d6be1514e128c55f24a27932a01592f
wuchengji <76484614@qq.com>
2018-11-20 12:06:25 +0800

    refactor: 修复优惠券为0不能选择问题

--- src/components/page/BrandProduct/GiftManage/AddGift.vue
+++ src/components/page/BrandProduct/GiftManage/AddGift.vue
@@ -198,22 +198,22 @@
             <el-tabs v-model="couponTypecouponType" v-loading="couponLoading" @tab-click="handleClick" style="height: 500px;overflow: auto">
                 <el-tab-pane label="满减券" name="1">
                     <div v-for="(v,k) in couponList" style="overflow: hidden; margin-bottom: 10px" :key="k">
-                        <span :class="{'selected-coupon':true,'active-selected':v.selected}" @click="selectCoupon(v)">{{v.name}} </span>
+                        <span :class="{'selected-coupon':true,'active-selected':v.selected}" @click="selectCoupon(v)">{{v.name}}{{v.num==0?'(暂无库存)':''}} </span>
                     </div>
                 </el-tab-pane>
                 <el-tab-pane label="折扣券" name="3">
                     <div v-for="(v,k) in couponList" style="overflow: hidden; margin-bottom: 10px" :key="k">
-                        <span :class="{'selected-coupon':true,'active-selected':v.selected}" @click="selectCoupon(v)">{{v.name}} </span>
+                        <span :class="{'selected-coupon':true,'active-selected':v.selected}" @click="selectCoupon(v)">{{v.name}}{{v.num==0?'(暂无库存)':''}} </span>
                     </div>
                 </el-tab-pane>
                 <el-tab-pane label="抵扣券" name="4">
                     <div v-for="(v,k) in couponList" style="overflow: hidden; margin-bottom: 10px" :key="k">
-                        <span :class="{'selected-coupon':true,'active-selected':v.selected}" @click="selectCoupon(v)">{{v.name}} </span>
+                        <span :class="{'selected-coupon':true,'active-selected':v.selected}" @click="selectCoupon(v)">{{v.name}}{{v.num==0?'(暂无库存)':''}} </span>
                     </div>
                 </el-tab-pane>
                 <el-tab-pane label="抵价券" name="2">
                     <div v-for="(v,k) in couponList" style="overflow: hidden; margin-bottom: 10px" :key="k">
-                        <span :class="{'selected-coupon':true,'active-selected':v.selected}" @click="selectCoupon(v)">{{v.name}} </span>
+                        <span :class="{'selected-coupon':true,'active-selected':v.selected}" @click="selectCoupon(v)">{{v.name}}{{v.num==0?'(暂无库存)':''}} </span>
                     </div>
                 </el-tab-pane>
             </el-tabs>
@@ -802,6 +802,7 @@
                         const obj = {
                             name: v.name,
                             id: v.id,
+                            num: v.totalNumber,
                             selected: false
                         };
                         for (let i = 0; i < tmp.length; i++) {
@@ -823,6 +824,7 @@
             //  选择优惠券
             selectCoupon(coupon) {
                 coupon.selected = true;
+                if (coupon.num == 0) return this.$message.warning('该优惠券库存为0！');
                 for (let i = 0; i < this.tmpCouponList.length; i++) {
                     if (this.tmpCouponList[i].id == coupon.id) {
                         coupon.selected = false;
