7b58190668946c1106f15cf8d82973b9f339392b
wuchengji <76484614@qq.com>
2018-11-21 15:55:13 +0800

    feat: 产品礼包，售后天数、重量长度限制

--- src/components/page/BrandProduct/GiftManage/AddGift.vue
+++ src/components/page/BrandProduct/GiftManage/AddGift.vue
@@ -406,8 +406,12 @@
                     this.$message.warning('请添加产品图片');
                     return false;
                 }
-                if (this.form.weight > 9999999999999999 || this.form.weight <= 0) {
+                if (this.form.weight > 999999999 || this.form.weight <= 0) {
                     this.$message.warning('请输入正确的产品重量!');
+                    return false;
+                }
+                if (this.form.aferServiceDays > 999999999 || this.form.aferServiceDays <= 0) {
+                    this.$message.warning('请输入正确的售后周期!');
                     return false;
                 }
                 // else if (this.selectedTagArr.length == 0) {
