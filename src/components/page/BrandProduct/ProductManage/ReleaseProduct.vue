679d8498b09d6b7bd2775427b6fc8c867764b570
wuchengji <76484614@qq.com>
2018-11-23 09:56:00 +0800

    feat: 修改产品重量售后周期提示语

--- src/components/page/BrandProduct/ProductManage/ReleaseProduct.vue
+++ src/components/page/BrandProduct/ProductManage/ReleaseProduct.vue
@@ -300,11 +300,12 @@
                     this.$message.warning('请添加产品图片');
                     return false;
                 }
-                if (this.form.weight > 999999999 || this.form.weight <= 0) {
-                    this.$message.warning('请输入正确的产品重量!');
+                const reg = /^(0|[1-9]\d*)([.]{1}[0-9]{1,2})?$/;
+                if (!reg.test(this.form.weight) || this.form.weight > 1000000000) {
+                    this.$message.error('重量不符合规则!');
                     return false;
                 }
-                if (this.form.afterSaleServiceDays > 999999999 || this.form.afterSaleServiceDays <= 0) {
+                if (this.form.afterSaleServiceDays > 999999999 || this.form.afterSaleServiceDays < 0) {
                     this.$message.warning('请输入正确的售后周期!');
                     return false;
                 }
