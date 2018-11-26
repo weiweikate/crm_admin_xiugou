eb1ceac40184c2ff28798a3b1af24ead6ba293a2
wuchengji <76484614@qq.com>
2018-11-26 18:34:49 +0800

    feat: 完成产品基础参数编辑

--- src/components/page/BrandProduct/ProductManage/productRelease/_baseParam.vue
+++ src/components/page/BrandProduct/ProductManage/productRelease/_baseParam.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="prod-base-param">
-        <el-form :model="form" :rules="rules" label-position="left" label-width="120px">
+        <el-form :model="form" :rules="rules" label-position="left" label-width="100px">
             <div class="pro-title">基础信息</div>
             <el-form-item prop="title" label="商品标题">
                 <el-input v-model="form.title"></el-input>
@@ -8,15 +8,93 @@
             <el-form-item prop="subtitle" label="商品副标题">
                 <el-input v-model="form.subtitle"></el-input>
             </el-form-item>
-            <el-form-item prop="supplier" label="选择供应商" inline>
-                <el-select v-model="form.supplier" placeholder="请选择供应商">
-                    <el-option v-for="(v, k) in supplierArr" :key="k" :label="v.label" :value="v.value"></el-option>
-                </el-select>
-                <el-form-item prop="supplier" label="选择供应商" inline-message="asdsada">
+            <el-form-item prop="supplier" label="选择供应商">
+                <el-col :span="11">
                     <el-select v-model="form.supplier" placeholder="请选择供应商">
                         <el-option v-for="(v, k) in supplierArr" :key="k" :label="v.label" :value="v.value"></el-option>
                     </el-select>
-                </el-form-item>
+                </el-col>
+                <el-col :span="11">
+                    <el-form-item prop="deliveryWarehouse" label="发货仓库">
+                        <el-select v-model="form.deliveryWarehouse" placeholder="请选择发货仓库">
+                            <el-option v-for="(v, k) in deliveryWarehouseArr" :key="k" :label="v.label" :value="v.value"></el-option>
+                        </el-select>
+                    </el-form-item>
+                </el-col>
+            </el-form-item>
+            <el-form-item prop="prodType" label="商品类型">
+                <el-col :span="11">
+                    <el-select v-model="form.prodType" placeholder="请选择商品类型">
+                        <el-option label="普通商品" value="1"></el-option>
+                        <el-option label="内购商品" value="2"></el-option>
+                        <!--<el-option label="虚拟商品" value="3"></el-option>-->
+                        <el-option label="卡券商品" value="4"></el-option>
+                    </el-select>
+                </el-col>
+                <el-col :span="11">
+                    <el-form-item prop="isProprietary" label="是否自营">
+                        <el-select v-model="form.isProprietary" placeholder="请选择是否自营">
+                            <el-option label="是" value="1"></el-option>
+                            <el-option label="否" value="2"></el-option>
+                        </el-select>
+                        <span style="color: #ccc">自营代表开票方为平台</span>
+                    </el-form-item>
+                </el-col>
+            </el-form-item>
+            <el-form-item prop="tradeType" label="贸易类型">
+                <el-col :span="11">
+                    <el-select v-model="form.tradeType" placeholder="请选择贸易类型">
+                        <el-option label="一般贸易" value="1"></el-option>
+                        <!--<el-option label="跨境保税" value="2"></el-option>-->
+                        <!--<el-option label="海外直邮" value="3"></el-option>-->
+                        <!--<el-option label="海淘" value="4"></el-option>-->
+                    </el-select>
+                </el-col>
+                <el-col :span="11">
+                    <el-form-item prop="rate" label="税率">
+                        <el-input class="pram-inp" v-model.number="form.rate">
+                            <template slot="append">%</template>
+                        </el-input>
+                    </el-form-item>
+                </el-col>
+            </el-form-item>
+            <el-form-item prop="brand" label="品牌">
+                <el-select class="search-inp" filterable placeholder="请选择品牌" v-model="form.brand">
+                    <el-option value="1">小米</el-option>
+                </el-select>
+            </el-form-item>
+            <div class="pro-title">自然属性</div>
+            <el-form-item>
+                <el-col :span="11" v-for="(v, k) in naturalAttribute" :key="k">
+                    <el-form-item :label="v.name">
+                        <el-select class="search-inp" filterable placeholder="请选择" v-model="v.value">
+                            <el-option v-for="(v1, k1) in v.options" :key="`${k}-${k1}`" :label="v1.label" :value="v1.value"></el-option>
+                        </el-select>
+                    </el-form-item>
+                </el-col>
+                <el-col :span="11">
+                    <div class="operate-natural-attr">
+                        <span>添加新属性</span>
+                        <span>|</span>
+                        <span>刷新</span>
+                    </div>
+                </el-col>
+            </el-form-item>
+            <div class="pro-title">销售属性</div>
+            <el-form-item v-for="(v, k) in salesAttrArr" :key="k" prop="brand" :label="v.name">
+                <div v-if="v.type == 1">
+                    <div class="img-type" v-for="(v1, k1) in v.options" :key="`${k}-${k1}`">
+                        <el-checkbox v-model="v1.value">{{v1.label}}</el-checkbox>
+                        <img :src="v1.imgUrl" alt="">
+                    </div>
+                    <div class="img-type" v-for="(v1, k1) in v.options" :key="`${k}-${k1}`">
+                        <el-checkbox v-model="v1.value">{{v1.label}}</el-checkbox>
+                        <img :src="v1.imgUrl" alt="">
+                    </div>
+                </div>
+                <div v-else-if="v.type == 2" class="sales-type">
+                    <el-checkbox v-for="(v1, k1) in v.options" :key="`${k}--${k1}`" v-model="v1.value">{{v1.label}}</el-checkbox>
+                </div>
             </el-form-item>
         </el-form>
     </div>
@@ -30,14 +108,29 @@
                 form: {
                     title: '',
                     subtitle: '',
-                    supplier: ''
+                    supplier: '',
+                    deliveryWarehouse: '',
+                    prodType: '',
+                    isProprietary: '',
+                    tradeType: '',
+                    rate: '',
+                    brand: ''
                 },
                 rules: {
                     title: [{ required: true, message: '请输入商品标题', trigger: 'blur' }, { min: 2, max: 46, message: '长度在 2 到 46 个字符', trigger: 'blur' }],
                     subtitle: [{ min: 2, max: 20, message: '长度在 2 到 20 个字符', trigger: 'blur' }],
-                    supplier: [{ required: true, message: '请选择供应商', trigger: 'blur' }]
+                    supplier: [{ required: true, message: '请选择供应商', trigger: 'blur' }],
+                    deliveryWarehouse: [{ required: true, message: '请选择发货仓库', trigger: 'blur' }],
+                    prodType: [{ required: true, message: '请选择商品类型', trigger: 'blur' }],
+                    isProprietary: [{ required: true, message: '请选择是否自营', trigger: 'blur' }],
+                    tradeType: [{ required: true, message: '请选择贸易类型', trigger: 'blur' }],
+                    rate: [{ required: true, type: 'number', message: '请输入正确的税率', trigger: 'blur' }],
+                    brand: [{ required: true, message: '请输入品牌', trigger: 'blur' }]
                 },
-                supplierArr: [{label: '小米批发商', value: '1'}]
+                supplierArr: [{ label: '小米批发商', value: '1' }],
+                deliveryWarehouseArr: [{ label: '平台发货', value: '1' }],
+                naturalAttribute: [{name: '尺寸', value: '', options: [{label: 'XL', value: '1'}]}, {name: '重量', value: '', options: [{label: '10kg', value: '1'}]}, {name: '颜色', value: '', options: [{label: '红色', value: '1'}]}],
+                salesAttrArr: [{name: '体积', type: '1', options: [{label: '大', value: false, imgUrl: 'https://mr-test-sg.oss-cn-hangzhou.aliyuncs.com/sharegoods/53c438c290b4430094a681c460dc28c8.jpg'}]}, {name: '规格', type: '2', options: [{label: '全网通', value: true}, {label: '联通', value: false}]}]
             };
         }
     };
@@ -53,6 +146,27 @@
             padding: 0 25px;
             box-sizing: border-box;
             margin-bottom: 20px;
+        }
+        .pram-inp{
+            width: 215px;
+        }
+        .operate-natural-attr{
+            cursor: pointer;
+            color: #33b4ff;
+            font-size: 12px;
+        }
+        .img-type{
+            width: 600px;
+            display: flex;
+            align-items: center;
+            margin-bottom: 10px;
+            img {
+                width: 50px;
+                height: 50px;
+                border-radius: 5px;
+                border: 1px solid #ccc;
+                margin-left: 50px;
+            }
         }
     }
 </style>
