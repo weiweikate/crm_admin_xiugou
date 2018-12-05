75f6ae7e900a2d7dce075d50fe7f9622147533ce
“chang.w” <185483658@qq.com>
2018-12-05 20:18:08 +0800

    refactor: 1、运费模板隐藏不支持配送区域

--- src/components/page/BrandProduct/BrandProductClassify/BrandProductClassify.vue
+++ src/components/page/BrandProduct/BrandProductClassify/BrandProductClassify.vue
@@ -74,7 +74,7 @@
                         <el-option label="否" value='2'></el-option>
                     </el-select>
                 </el-form-item>
-                <el-form-item prop="type" label="类目类型">
+                <el-form-item  prop="type" label="类目类型">
                     <el-select v-model="form.type">
                         <el-option label="普通类目" value='1'></el-option>
                         <el-option label="隐藏类目" value='2'></el-option>
