379e97d59f141543b58ac26dcfffc3f96a66c446
chang.w <wangchang@meeruu.com>
2018-12-27 11:58:40 +0800

    fix: 供应商仓虚拟仓更换位置

--- src/components/page/OrderManage/OrderManage/OrderList.vue
+++ src/components/page/OrderManage/OrderManage/OrderList.vue
@@ -97,12 +97,12 @@
 </template>
 
 <script>
-import vBreadcrumb from "@/components/common/Breadcrumb.vue";
-import moment from "moment";
-import { myMixinTable } from "@/JS/commom";
-import vOrderlist from "./_orderList/_orderList";
-import * as api from "@/api/api.js";
-import utils from "@/utils/index.js";
+import vBreadcrumb from '@/components/common/Breadcrumb.vue';
+import moment from 'moment';
+import { myMixinTable } from '@/JS/commom';
+import vOrderlist from './_orderList/_orderList';
+import * as api from '@/api/api.js';
+import utils from '@/utils/index.js';
 
 export default {
     components: {
@@ -114,71 +114,71 @@ export default {
 
     data() {
         return {
-            nav: ["订单管理", "订单管理"],
-            activeName: "all",
+            nav: ['订单管理', '订单管理'],
+            activeName: 'all',
             dateRange: [],
             repertoryArr: [
                 // 发货仓库
-                { label: "自建仓", value: "1" },
-                { label: "加盟仓", value: "2" },
-                { label: "供应商仓", value: "3" }
-                // { label: '虚拟仓', value: '4' }
+                { label: '自建仓', value: '1' },
+                { label: '加盟仓', value: '2' },
+                { label: '供应商仓', value: '4' },
+                { label: '虚拟仓', value: '3' }
             ],
             orderSourceArr: [
                 // 订单来源
-                { label: "小程序", value: "1" },
-                { label: "APP", value: "2" },
-                { label: "微商城", value: "3" }
+                { label: '小程序', value: '1' },
+                { label: 'APP', value: '2' },
+                { label: '微商城', value: '3' }
             ],
             starArr: [
                 // 订单标记
-                { label: "红色标记", value: "1" },
-                { label: "蓝色标记", value: "2" },
-                { label: "绿色标记", value: "3" },
-                { label: "黄色标记", value: "4" },
-                { label: "紫色标记", value: "5" }
+                { label: '红色标记', value: '1' },
+                { label: '蓝色标记', value: '2' },
+                { label: '绿色标记', value: '3' },
+                { label: '黄色标记', value: '4' },
+                { label: '紫色标记', value: '5' }
             ],
             sendStatusArr: [
                 // 推送状态
-                { label: "无需推送", value: "1" },
-                { label: "等待推送", value: "2" },
-                { label: "推送成功", value: "3" },
-                { label: "推送失败", value: "4" }
+                { label: '无需推送', value: '0' },
+                { label: '等待推送', value: '1' },
+                { label: '推送成功', value: '3' },
+                { label: '推送失败', value: '2' }
             ],
             lockStatusArr: [
                 // 锁定状态
-                { label: "无需锁定", value: "2" },
-                { label: "未锁定", value: "0" },
-                { label: "已锁定", value: "1" }
+                { label: '无需锁定', value: '2' },
+                { label: '未锁定', value: '0' },
+                { label: '已锁定', value: '1' }
             ],
             orderStatusArr: [
                 // 订单状态
-                { label: "待付款", value: "1" },
-                { label: "已付款", value: "2" },
+                { label: '待付款', value: '1' },
+                { label: '已付款', value: '2' },
                 // { label: '部分发货', value: '6' },
-                { label: "已发货", value: "3" },
-                { label: "交易完成", value: "4" },
-                { label: "交易关闭", value: "5" }
+                { label: '已发货', value: '3' },
+                { label: '交易完成', value: '4' },
+                { label: '交易关闭', value: '5' }
             ],
             form: {
-                platformNo: "", // 平台级订单号
-                warehouseOrderNo: "", // 仓库级订单号
-                warehouseType: "", // 发货仓库
-                source: "", // 订单来源
-                invoiceRequired: "", // 开具发票
-                userPhone: "", // 用户账号
-                userPhoneName: "", // 收货人姓名
-                marker: "", // 标记
-                pushStatus: "", // 推送状态
-                lockStatus: "", // 锁定状态
-                orderStatus: "" // 订单状态
+                platformNo: '', // 平台级订单号
+                warehouseOrderNo: '', // 仓库级订单号
+                warehouseType: '', // 发货仓库
+                source: '', // 订单来源
+                invoiceRequired: '', // 开具发票
+                userPhone: '', // 用户账号
+                userPhoneName: '', // 收货人姓名
+                marker: '', // 标记
+                pushStatus: '', // 推送状态
+                lockStatus: '', // 锁定状态
+                orderStatus: '' // 订单状态
             },
             formData: {}
         };
     },
 
     mounted() {
-        this.handleClick({ name: "all" });
+        this.handleClick({ name: 'all' });
     },
 
     methods: {
@@ -195,39 +195,20 @@ export default {
                 pushStatus: this.form.pushStatus,
                 lockStatus: this.form.lockStatus,
                 orderStatus: this.form.orderStatus,
-                form:
-                    this.dateRange.length !== 0
-                        ? moment(this.dateRange[0]).format(
-                              "YYYY-MM-DD 00:00:00"
-                          )
-                        : "",
-                to:
-                    this.dateRange.length !== 0
-                        ? moment(this.dateRange[1]).format(
-                              "YYYY-MM-DD 00:00:00"
-                          )
-                        : "",
-                warehouseOrderNos: this.$refs[
-                    this.activeName
-                ].warehouseOrderNos.join(",")
+                form: this.dateRange.length !== 0 ? moment(this.dateRange[0]).format('YYYY-MM-DD 00:00:00') : '',
+                to: this.dateRange.length !== 0 ? moment(this.dateRange[1]).format('YYYY-MM-DD 00:00:00') : '',
+                warehouseOrderNos: this.$refs[this.activeName].warehouseOrderNos.join(',')
             };
-            this.downloadOrderList =
-                api.downloadOrderList + "?" + utils.setRequestParams(params);
+            this.downloadOrderList = api.downloadOrderList + '?' + utils.setRequestParams(params);
             this.$refs.exportData.href = this.downloadOrderList;
         },
         // 提交表单
         getList() {
             const data = {};
             Object.assign(data, this.form);
-            data.from =
-                this.dateRange.length != 0
-                    ? moment(this.dateRange[0]).format("YYYY-MM-DD 00:00:00")
-                    : "";
-            data.to =
-                this.dateRange.length != 0
-                    ? moment(this.dateRange[1]).format("YYYY-MM-DD 23:59:59")
-                    : "";
-            data.orderStatus = this.activeName === "all" ? "" : this.activeName;
+            data.from = this.dateRange.length !== 0 ? moment(this.dateRange[0]).format('YYYY-MM-DD 00:00:00') : '';
+            data.to = this.dateRange.length !== 0 ? moment(this.dateRange[1]).format('YYYY-MM-DD 23:59:59') : '';
+            data.orderStatus = this.activeName === 'all' ? '' : this.activeName;
             this.$refs[this.activeName].page.currentPage = 1;
             this.$refs[this.activeName].data = data;
             this.$refs[this.activeName].getList(this.page.currentPage);
@@ -236,20 +217,19 @@ export default {
         resetForm(formName) {
             this.$refs[formName].resetFields();
             this.dateRange = [];
-            this.getList({ name: "all" });
+            this.getList({ name: 'all' });
         },
         //  点击tab选项卡
         handleClick(tab) {
-            this.resetForm("form");
+            this.resetForm('form');
             this.activeName = tab.name;
             this.getList();
         },
         // 推送
         sendOut() {
-            const warehouseOrderNos = this.$refs[this.activeName]
-                .warehouseOrderNos;
+            const warehouseOrderNos = this.$refs[this.activeName].warehouseOrderNos;
             if (!warehouseOrderNos.length) {
-                return this.$message.warning("请选择订单");
+                return this.$message.warning('请选择订单');
             } else {
                 this.$refs[this.activeName].pushCloud();
             }
