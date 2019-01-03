a501887bdf60138d3907da80a8ab91a6597cd17b
chang.w <wangchang@meeruu.com>
2019-01-03 14:52:50 +0800

    refactor: 运费模板重构

--- src/components/page/BrandProduct/ShippingTemplate/ShippingTemplate.vue
+++ src/components/page/BrandProduct/ShippingTemplate/ShippingTemplate.vue
@@ -1,77 +1,63 @@
 <template>
     <div class="freight-template">
-        <v-breadcrumb :nav="['品牌产品管理','运费模板']"></v-breadcrumb>
-        <div class="table-block">
+        <v-breadcrumb :nav="['物流管理','运费模板']"></v-breadcrumb>
+        <div class="table-block" v-loading="btnLoading">
             <el-button type="primary" style="margin-bottom: 20px" @click="addTemplate" v-auth="'brand.shippingTemplate.tj'">添加模板</el-button>
             <template>
-                <table v-for="(v,k) in mockData" :key="k" class="table-area">
+                <div v-if="tableData.length==0" class="nodata">暂无模板~</div>
+                <table v-else v-for="(v,k) in tableData" :key="k" class="table-area">
                     <tr>
                         <td colspan="2" class="left">
                             <span>id:{{v.id}}</span>
-                            <span class="name">{{v.name}}<span v-if="v.type==0">（已指定条件包邮）</span></span>
+                            <span class="name">{{v.name}}<span v-if="v.hasExemption==1">（已指定条件包邮）</span></span>
                         </td>
                         <td colspan="3" class="right">
-                            <span>{{v.adminName}}</span>
+                            <span>{{v.createAdmin}}</span>
                             <span class="time">更新于 {{v.updateTime|formatDateAll}}</span>
-                            <el-button type="primary" v-if="v.status==0">启用</el-button>
-                            <el-button type="primary" v-if="v.status==1">停用</el-button>
-                            <el-button type="success">修改</el-button>
-                            <el-button type="warning">删除</el-button>
+                            <el-button type="primary" v-if="v.status==0" @click="openOrClose(v.id,0)">启用</el-button>
+                            <el-button type="danger" v-if="v.status==1" @click="openOrClose(v.id,1)">停用</el-button>
+                            <el-button type="success" @click="editItem(v.id,v.num,2)">修改</el-button>
+                            <el-button type="warning" @click="delItem(v.id)">删除</el-button>
                         </td>
                     </tr>
                     <tr class="table-header">
                         <td>配送地址</td>
-                        <td>{{tableHeader[v.style].unit}}</td>
+                        <td>{{tableHeader[v.calcType-1].unit}}</td>
                         <td>首费(元)</td>
-                        <td>{{tableHeader[v.style].nextUnit}}</td>
+                        <td>{{tableHeader[v.calcType-1].nextUnit}}</td>
                         <td>续费(元)</td>
                     </tr>
-                    <tr v-for="(v1,k1) in v.freightList" :key="k1">
-                        <td class="city">{{v1.city}}</td>
-                        <td>{{v1.unit}}</td>
-                        <td>{{v1.unitPrice}}</td>
+                    <tr v-for="(v1,k1) in v.freightTemplateInfoList" :key="k1">
+                        <td class="city">
+                            <div v-for="(v2,k2) in v1.freightTemplateInfoDetailList" :key="k2">
+                                <span>{{v2.provinceName}}:</span>
+                                <span>{{v2.cityNames}}</span>
+                            </div>
+                        </td>
+                        <td>{{v1.startUnit}}</td>
+                        <td>{{v1.startPrice}}</td>
                         <td>{{v1.nextUnit}}</td>
-                        <td>{{v1.nextUnitPrice}}</td>
+                        <td>{{v1.nextPirce}}</td>
                     </tr>
                 </table>
-                <el-table v-loading="tableLoading" :data="tableData" border style="width: 100%">
-                    <el-table-column type="index" label="运费模板编号" width="100" align="center"></el-table-column>
-                    <el-table-column prop="name" label="模板名称" align="center"></el-table-column>
-                    <el-table-column label="是否包邮" align="center">
-                        <template slot-scope="scope">
-                            <template v-if="scope.row.freightType==1">自定义运费</template>
-                            <template v-if="scope.row.freightType==2">平台承担运费</template>
-                            <template v-if="scope.row.freightType==3">满{{scope.row.freightFreePrice}}包邮</template>
-                        </template>
-                    </el-table-column>
-                    <el-table-column label="计价方式" align="center">
-                        <template slot-scope="scope">
-                            <template v-if="scope.row.calcType==1">按重量</template>
-                            <template v-if="scope.row.calcType==2">按体积</template>
-                            <template v-if="scope.row.calcType==3">按件数量</template>
-                        </template>
-                    </el-table-column>
-                    <!-- <el-table-column label="状态" width="180" align="center">
-                        <template slot-scope="scope">
-                            <template v-if="scope.row.status == 1">启用</template>
-                            <template v-if="scope.row.status == 2">停用</template>
-                        </template>
-                    </el-table-column>-->
-                    <el-table-column prop="num" label="使用数" align="center"></el-table-column>
-                    <el-table-column min-width="100" label="模板设置" align="center">
-                        <template slot-scope="scope">
-                            <!--<el-button type="primary" size="small">设为默认</el-button>-->
-                            <el-button type="warning" size="small" @click="editItem(scope.$index,scope.row.id)" v-auth="'brand.shippingTemplate.sz'">设置模板
-                            </el-button>
-                            <el-button type="danger" v-if="scope.row.num==0" size="small" @click="delItem(scope.$index,scope.row.id)" v-auth="'brand.shippingTemplate.sc'">删除模板
-                            </el-button>
-                        </template>
-                    </el-table-column>
-                </el-table>
+                <div class="block">
+                    <el-pagination background @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="page.currentPage" :page-size="page.pageSize" layout="total, prev, pager, next, jumper" :total="page.totalPage">
+                    </el-pagination>
+                </div>
             </template>
         </div>
         <!--删除弹窗-->
         <delete-toast :id='delId' :url='delUrl' :uri='delUri' @msg='deleteToast' v-if="isShowDelToast"></delete-toast>
+        <!-- 停用启用修改弹窗 -->
+        <el-dialog title="温馨提示" :visible.sync="tipMask">
+            <div class="tip-content">
+                <div class="tip-info">{{tipInfo[index]}}</div>
+                <div>
+                    <el-button type="primary" @click="sure">确认</el-button>
+                    <el-button type="success" @click="tipMask=false">取消</el-button>
+                </div>
+            </div>
+        </el-dialog>
     </div>
 </template>
 
@@ -79,7 +65,7 @@
 import vBreadcrumb from '@/components/common/Breadcrumb.vue';
 import icon from '@/components/common/ico.vue';
 import deleteToast from '@/components/common/DeleteToast';
-import utils from '@/utils/index.js';
+// import utils from '@/utils/index.js';
 import * as pApi from '@/privilegeList/ShippingTemplate/index.js';
 import { myMixinTable } from '@/JS/commom';
 import request from '@/http/http.js';
@@ -106,32 +92,44 @@ export default {
             delId: '',
             delUrl: '',
             delUri: '',
-            mockData: [
+            /*   mockData: [
                 { id: 1, name: '名字1', type: 0, adminName: 'cc', updateTime: 12823484344, status: 0, style: 0, freightList: [{ city: '中国', unit: 1, unitPrice: 1, nextUnit: 1, nextUnitPrice: 1 }, { city: '杭州市', unit: 1, unitPrice: 10, nextUnit: 1, nextUnitPrice: 8 }] },
                 { id: 2, name: '名字2', type: 1, adminName: 'cc', updateTime: 15000000000, status: 1, style: 1, freightList: [{ city: '中国', unit: 1, unitPrice: 1, nextUnit: 1, nextUnitPrice: 1 }, { city: '杭州市,宁波市，湖州市，绍兴市，金华市，诸暨市，宁波市，湖州市，绍兴市，金华市，诸暨市', unit: 1, unitPrice: 10, nextUnit: 1, nextUnitPrice: 8 }] },
                 { id: 3, name: '名字3', type: 1, adminName: 'cc', updateTime: 15000000000, status: 1, style: 2, freightList: [{ city: '中国', unit: 1, unitPrice: 1, nextUnit: 1, nextUnitPrice: 1 }, { city: '杭州市', unit: 1, unitPrice: 10, nextUnit: 1, nextUnitPrice: 8 }] }
-            ],
-            tableHeader: [{ unit: '首件(件)', nextUnit: '续件(件)' }, { unit: '首体积(m³)', nextUnit: '续体积(m³)' }, { unit: '首重(kg)', nextUnit: '续重(kg)' }]
+            ], */
+            tableHeader: [{ unit: '首重(kg)', nextUnit: '续重(kg)' }, { unit: '首体积(m³)', nextUnit: '续体积(m³)' }, { unit: '首件(件)', nextUnit: '续件(件)' }],
+            tipInfo: ['确认停用？', '确认启用？', '存在商品使用该运费模板，调整后立即生效'],
+            index: 0,
+            tipMask: false,
+            id: ''
         };
     },
     created() {},
     activated() {
-        this.getList();
+        this.getList(this.page.currentPage);
     },
     methods: {
         // 获取列表
-        getList() {
-            const that = this;
-            that.tableLoading = true;
+        getList(val) {
+            const data = {
+                page: this.page.currentPage,
+                pageSize: this.page.pageSize
+            };
+            this.page.currentPage = val;
+            this.tableLoading = true;
             request
-                .queryFreightTemplateList({})
+                .queryFreightTemplateList(data)
                 .then(res => {
-                    that.tableLoading = false;
-                    that.tableData = res.data;
+                    this.tableLoading = false;
+                    if (!res.data.data) {
+                        return;
+                    }
+                    this.tableData = res.data.data;
+                    this.page.totalPage = res.data.totalNum;
                 })
                 .catch(error => {
                     console.log(error);
-                    that.tableLoading = false;
+                    this.tableLoading = false;
                 });
         },
 
@@ -140,17 +138,18 @@ export default {
             this.$router.push({ path: '/addTemplate' });
         },
         // 编辑
-        editItem(index, id) {
-            sessionStorage.setItem('templateId', id);
-            this.$router.push({ path: '/editTemplate', query: { templateId: id } });
+        // num模板被应用次数 0未被使用，直接跳到编辑页面，否则弹窗确认后跳转
+        editItem(id, num, index) {
+            this.id = id;
+            if (num) {
+                this.index = index;
+                this.tipMask = true;
+            } else {
+                this.toEditPage(id);
+            }
         },
-
-        // 跳到品牌页面
-        // toBrand(index,row){
-        //     this.$router.push({ path: "/addOrUpBrand", query: { params: row } });
-        // },
         // 删除
-        delItem(index, id) {
+        delItem(id) {
             this.delId = id;
             this.delUrl = 'deleteFreightTemplateById';
             this.delUri = pApi.deleteFreightTemplateById;
@@ -159,7 +158,26 @@ export default {
         // 删除弹窗
         deleteToast(msg) {
             this.isShowDelToast = msg;
-            this.getList();
+            this.getList(this.page.currentPage);
+        },
+        // 停用启用
+        openOrClose(id, index) {
+            this.index = index;
+            this.tipMask = true;
+        },
+        // 弹窗确认
+        sure() {
+            if (this.index === 2) {
+                // 修改
+                this.toEditPage(this.id);
+            } else {
+                console.log(111)
+            }
+        },
+        // 跳转到编辑页面
+        toEditPage(id) {
+            sessionStorage.setItem('templateId', id);
+            this.$router.push({ path: '/editTemplate', query: { templateId: id }});
         }
     }
 };
@@ -173,6 +191,13 @@ export default {
         border-radius: 5px;
         vertical-align: middle;
     }
+    .nodata {
+        text-align: center;
+        height: 100px;
+        line-height: 100px;
+        border: 1px solid #eee;
+        color: #303133;
+    }
     /*表格样式*/
     .table-block {
         padding: 20px 20px 60px;
@@ -182,7 +207,14 @@ export default {
         float: right;
         margin-top: 10px;
     }
-
+    .tip-content {
+        text-align: center;
+    }
+    .tip-info {
+        height: 70px;
+        line-height: 40px;
+        font-size: 22px;
+    }
     /*弹窗样式*/
     .el-dialog {
         width: 530px;
@@ -246,6 +278,12 @@ export default {
         }
         .table-header {
             background: #f7f7f7;
+            td {
+                width: 15%;
+            }
+            td:first-child {
+                width: 40%;
+            }
         }
         .city {
             width: 600px;
