86b902352cd0ab413932ec24d345e45a54d99ab3
jiangguyu <jijiangguyu@meeruu.comangguyu>
2019-01-21 15:50:51 +0800

    添加dom循环key:index

--- src/components/page/OperateManage/Experience/expAddOrEdit.vue
+++ src/components/page/OperateManage/Experience/expAddOrEdit.vue
@@ -11,7 +11,7 @@
                 <el-form-item prop="rules" label="活动规则">
                     <el-button size="big" :type="form.rules.length >= 5 ? 'info' : 'danger'" @click="addRegular">添加区间</el-button> <span style="color:red" v-if="form.rules.length >= 5">最多添加5个区间</span>
                     <div class="regular-list mt20">
-                        <div class="regular-item mt10" v-for="(item,index) in form.rules">
+                        <div class="regular-item mt10" v-for="(item,index) in form.rules" :key="index">
                             购满买 <el-input-number
                                     :controls="false"
                                     v-model.number="item.startPrice"
@@ -225,7 +225,7 @@
                 <el-button type="danger" @click="getList(1)">搜索</el-button>
             </el-form>
             <div class="dialog-body" v-loading="searchLoading">
-                <div class="dialog-good-item" v-for="(item,index) in searchGoods" @click="toggleGoodChecked(index)">
+                <div class="dialog-good-item" v-for="(item,index) in searchGoods" @click="toggleGoodChecked(index)" :key="index">
                     <img :src="item.imgUrl || '/static/img/defaultImg.png'" alt="good img">
                     <div class="dialog-good-name">{{item.name}}</div>
                     <div class="dialog-good-shade" v-if="item.checked">已选中</div>
@@ -264,7 +264,7 @@
         <el-dialog title="商品添加失败" :visible.sync="errorDialog" center @close="dealErrSpuGoods">
             红字的为SPU导入失败异常原因, 关闭窗口后剔除错误商品
             <div class="err-list">
-                <div class="err-item" v-for="item in checkActivityProductRes">
+                <div class="err-item" v-for="(item,index) in checkActivityProductRes" :key="index">
                     <span class="err-item-code">{{item.spuCode}}</span><span class="inline red">{{item.errMsg}}</span>
                 </div>
             </div>
